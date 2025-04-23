3199eee16bfe083e32d3983e6906cd149bcd20e4
Kacper <melchior2.km@gmail.com>
2025-04-24 01:09:12 +0200

    Dodano podstawowy interfejs użytkownika z możliwością dodawania i wyświetlania zadań. Rozbudowano funkcje zarządzania plikiem Excel

--- functions.py
+++ functions.py
@@ -1,7 +1,14 @@
 import openpyxl
+from openpyxl.styles import PatternFill
+from tasks import TodoTask
 
-# Stałe
 NAZWA_PLIKU = "Tasks.xlsx"
+KOLORY = {
+    "Do zrobienia": "#FF9999",
+    "W trakcie": "#FFFF99",
+    "Wykonane": "#99FF99"
+}
+
 
 def sprawdz_lub_utworz_plik():
     """Sprawdza istnienie pliku Excel lub tworzy nowy z nagłówkami."""
@@ -15,5 +22,40 @@ def sprawdz_lub_utworz_plik():
         skoroszyt.save(NAZWA_PLIKU)
     return skoroszyt
 
-# TODO: Zaimplementować funkcję ładowania zadań
-# TODO: Dodać funkcje zarządzania zadaniami (dodawanie, zmiana statusu)
+
+def zaladuj_zadania():
+    """Ładuje listę zadań z pliku Excel."""
+    skoroszyt = sprawdz_lub_utworz_plik()
+    arkusz = skoroszyt.active
+    zadania = []
+
+    for wiersz in arkusz.iter_rows(min_row=2, values_only=True):
+        tytul, status = wiersz
+        if status == "Do zrobienia":
+            zadania.append(TodoTask(tytul))
+        # TODO: Obsłużyć inne statusy zadań
+
+    return zadania
+
+
+def dodaj_zadanie(tytul):
+    """Dodaje nowe zadanie do pliku."""
+    zadania = zaladuj_zadania()
+    zadania.append(TodoTask(tytul))
+    zapisz_zadania(zadania)
+
+
+def zapisz_zadania(zadania):
+    """Zapisuje listę zadań do pliku Excel."""
+    skoroszyt = openpyxl.Workbook()
+    arkusz = skoroszyt.active
+    arkusz.title = "Tasks"
+    arkusz.append(["Title", "Status"])
+
+    for zadanie in zadania:
+        arkusz.append([zadanie.tytul, zadanie.status])
+
+    skoroszyt.save(NAZWA_PLIKU)
+
+# TODO: Dodać funkcje zmiany statusu zadań
+# TODO: Dodać formatowanie komórek Excel
